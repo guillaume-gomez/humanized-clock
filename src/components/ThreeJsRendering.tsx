5809c85ae071db6aba6de1075428955197ebdc32
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-05-06 09:14:30 +0200

    create lib to facilitate to build the city$

--- src/components/ThreeJsRendering.tsx
+++ src/components/ThreeJsRendering.tsx
@@ -1,7 +1,8 @@
 import { useRef , useState, Suspense, MutableRefObject, useEffect } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { CameraControls, Stats, GizmoHelper, GizmoViewport, Center } from '@react-three/drei';
+import { CameraControls, Stats, GizmoHelper, GizmoViewport, Center, Grid, Stars } from '@react-three/drei';
 import LettersGrid from "./LettersGrid";
+import CityText from "./CityText";
 import { humanizedClockInFrench } from "../humanizedClock";
 
 interface ThreejsRenderingProps {
@@ -49,18 +50,22 @@ function ThreejsRendering({} : ThreejsRenderingProps) {
             <ambientLight intensity={0.5} />
             <directionalLight position={[10, 10, 10]} />
             <color attach="background" args={['#c0d6e9']} />
+            {/*<Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={1} />*/}
             { import.meta.env.MODE === "development" ? <Stats/> : <></> }
             <Center>
-              <LettersGrid jk dateHumanized={"il est " + humanizedClockInFrench(date)} />
+              {/*<LettersGrid jk dateHumanized={"il est " + humanizedClockInFrench(date)} />*/}
+              <CityText />
             </Center>
+            <Grid args={[50, 50]} position={[0,0,0]} sectionColor={'blue'} cellColor='black' />
+
             <GizmoHelper alignment="bottom-right" margin={[50, 50]}>
               <GizmoViewport labelColor="white" axisHeadScale={1} />
             </GizmoHelper>
             <CameraControls
-              minPolarAngle={0}
+              /*minPolarAngle={0}
               maxPolarAngle={Math.PI / 1.9}
               minAzimuthAngle={-0.55}
-              maxAzimuthAngle={0.55}
+              maxAzimuthAngle={0.55}*/
               makeDefault
               ref={cameraControlRef}
             />
@@ -70,5 +75,8 @@ function ThreejsRendering({} : ThreejsRenderingProps) {
     </div>
   );
 }
+
+
+
 
 export default ThreejsRendering;
---
+++ src/numberToWord.ts
@@ -0,0 +1,35 @@
+const TEN = 10;
+const ONE_HUNDRED = 100;
+
+const LESS_THAN_TWENTY = [
+    'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+    'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+];
+
+const TENTHS_LESS_THAN_HUNDRED = [
+    'zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+];
+
+export function generateWords(number: number): string {
+    var remainder, word,
+        words = arguments[1];
+
+    if (number === 0 || number < 0) {
+        return 'zero';
+    }else if (number < 20) {
+        return LESS_THAN_TWENTY[number];
+
+    } else if (number < ONE_HUNDRED) {
+        const remainder = number % TEN;
+        const word = TENTHS_LESS_THAN_HUNDRED[Math.floor(number / TEN)];
+        // In case of remainder, we need to handle it here to be able to add the “-”
+        if (remainder) {
+            return word + ' ' + LESS_THAN_TWENTY[remainder];
+        } else {
+            return word;
+        }
+    }
+    else {
+        return 'zero';
+    }
+}
