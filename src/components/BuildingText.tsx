330d14e15631a96d48b6c4e2867ddbf8b5b2faa8
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-05-08 12:40:05 +0200

    optimize the perfs

--- src/components/BuildingText.tsx
+++ src/components/BuildingText.tsx
@@ -1,25 +1,42 @@
+import { MeshStandardMaterial } from "three";
 import {  Text3D, Box } from '@react-three/drei';
-import { useRef, useEffect, useState } from "react";
+
+import { useRef, useEffect, useState, useMemo } from "react";
 import {Vector3, Box3} from 'three';
+
+import { extend } from '@react-three/fiber'
+import Myfont from "../5Identification-Mono.json"
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
+extend({ TextGeometry })
+
 
 interface BuildingTextProps {
     text: string;
     textNumber: string;
     position: [number, number, number];
     size?: number;
+    highlight?: boolean;
 
 }
 
-const font = "/5Identification-Mono.json";
+
 const letterSpacing = 0.05;
 
-function BuildingText({text, textNumber, position, size = 1} : BuildingTextProps) {
+function BuildingText({text, textNumber, position, size = 1, highlight = false} : BuildingTextProps) {
     // allow us to get the size of a text 3D to compute the size of the box
     const refText = useRef(null);
     const refNumber = useRef(null);
     const [boxSize, setBoxSize] = useState<[number, number, number]>([0,0,0]);
     const [textSize, setTextSize] = useState<[number, number, number]>([0,0,0]);
     const [numberSize, setNumberSize] = useState<[number, number, number]>([0,0,0]);
+
+    //create geometry and material to reuse it
+    const color = highlight ? 0xFDDE55 : "black";
+    const textMaterial = useMemo(() => new MeshStandardMaterial({color: color}), [color]);
+
+    const font = new FontLoader().parse(Myfont);
+    const geometry = useMemo(() => new TextGeometry(text, {font, size, letterSpacing, height:0.2}), [text]);
 
     useEffect(() => {
         if(refText.current) {
@@ -42,11 +59,6 @@ function BuildingText({text, textNumber, position, size = 1} : BuildingTextProps
         if(refNumber.current) {
             // Compute the bounding box
             let bbox = new Box3().setFromObject(refNumber.current);
-            // Init your size variable
-            const bboxSize = new Vector3(bbox);
-            // Get the size
-            bbox.getSize(bboxSize);
-
             const width = bbox.max.x - bbox.min.x;
             const height = bbox.max.y - bbox.min.y;
             const depth = bbox.max.z - bbox.min.z;
@@ -55,62 +67,51 @@ function BuildingText({text, textNumber, position, size = 1} : BuildingTextProps
     }, [refNumber.current, textNumber]);
 
 
+
+
     return(
         <group position={[position[0], position[1] + textSize[1], position[2]]} rotation={[0,0,-Math.PI/2]}>
-            <Box args={boxSize} position={[boxSize[0]/ 2, 0, 0]} material-color="black"/>
-            <Text3D
+            <Box args={boxSize} position={[boxSize[0]/ 2, 0, 0]} material-color={"black"}/>
+
+            <mesh
                 ref={refText}
-                letterSpacing={letterSpacing}
-                size={size}
-                font={font}
                 position={[0,-textSize[0]/2, size ]}
-            >
-                {text}
-                <meshStandardMaterial color={"green"} />
-            </Text3D>
+                material={textMaterial}
+                geometry={geometry}
+            />
 
-            <Text3D
-                letterSpacing={letterSpacing}
-                size={size}
-                font={font}
-                position={[0,-textSize[0]/2, -size - textSize[2]]}
-            >
-                {text}
-                <meshStandardMaterial color={"blue"} />
-            </Text3D>
+            <mesh
+                ref={refText}
+                rotation={[-Math.PI, 0,0]}
+                position={[0,textSize[0]/2, -size - textSize[2]]}
+                material={textMaterial}
+                geometry={geometry}
+            />
 
-            <Text3D
-                letterSpacing={letterSpacing}
-                size={size}
-                font={font}
-                position={[0,size +textSize[2],-textSize[0]/2]}
-                rotation={[Math.PI/2,0,0]}
-            >
-                {text}
-                <meshStandardMaterial color={"purple"} />
-            </Text3D>
+            <mesh
+                position={[0,size +textSize[2],textSize[0]/2]}
+                rotation={[-Math.PI/2,0,0]}
+                material={textMaterial}
+                geometry={geometry}
+            />
 
-            <Text3D
-                letterSpacing={letterSpacing}
-                size={size}
-                font={font}
+            <mesh
                 position={[0,-size,-textSize[0]/2]}
                 rotation={[Math.PI/2, 0, 0]}
-            >
-                {text}
-                <meshStandardMaterial color={"red"} />
-            </Text3D>
+                material={textMaterial}
+                geometry={geometry}
+            />
 
             <Text3D
                 ref={refNumber}
                 letterSpacing={0}
                 size={size * 0.60}
-                font={font}
+                font={Myfont}
                 position={[0,-numberSize[0]/2, numberSize[2]/2]}
                 rotation={[0,-Math.PI/2,Math.PI/2]}
+                material={textMaterial}
             >
                 {textNumber}
-                <meshStandardMaterial color={"orange"} />
             </Text3D>
         </group>
     );
