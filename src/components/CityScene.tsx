c28e3186b30e9dc725bfeeddbee014e4235d35b2
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-05-21 09:18:48 +0200

    fix indent

--- src/components/CityScene.tsx
+++ src/components/CityScene.tsx
@@ -16,23 +16,28 @@ function CityScene({date} : CitySceneProps) {
 
     return (
         <>
-        <ambientLight intensity={0.8} />
-        <directionalLight position={[10, 10, 10]} />
-        <color attach="background" args={['#111']} />
+            <ambientLight intensity={0.8} />
+            <directionalLight position={[10, 10, 10]} />
+            <color attach="background" args={['#111']} />
 
-        <Bounds>
-            <Center disableY>
-                <CityText seconds={date.getSeconds()} minutes={date.getMinutes()} hours={date.getHours()} />
-            </Center>
-        </Bounds>
+            <Bounds>
+                <Center disableY>
+                    <CityText
+                        seconds={date.getSeconds()}
+                        minutes={date.getMinutes()}
+                        hours={date.getHours()}
+                    />
+                </Center>
+            </Bounds>
 
-        <Grid args={[100,100]} position={[0,0,0]} sectionColor={'blue'} cellColor='black' />
+            <Grid args={[100,100]} position={[0,0,0]} sectionColor={'blue'} cellColor='black' />
 
-        <GizmoHelper alignment="bottom-right" margin={[50, 50]}>
-        <GizmoViewport labelColor="white" axisHeadScale={1} />
-        </GizmoHelper>
-        <CameraControls ref={cameraControlRef} makeDefault />
-    </>)
+            <GizmoHelper alignment="bottom-right" margin={[50, 50]}>
+            <GizmoViewport labelColor="white" axisHeadScale={1} />
+            </GizmoHelper>
+            <CameraControls ref={cameraControlRef} makeDefault />
+        </>
+    )
 }
 
 export default CityScene;
