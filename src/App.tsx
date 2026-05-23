8e00bbbfc07729074219fa329b8b636f50760840
Guillaume Gomez <guillaume.gomez846@gmail.com>
2026-05-23 11:12:14 +0000

    remove default code from the vite template

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,6 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
       <ThreeJsRendering date={new Date()}/>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
