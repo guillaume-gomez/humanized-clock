551dbbde7f763fb7d50ad8dc946dc3701de50dae
Guillaume Gomez <guillaume.gomez846@gmail.com>
2026-07-10 19:30:59 +0200

    fix signature of text geometry

--- src/components/BuildingText.tsx
+++ src/components/BuildingText.tsx
@@ -45,7 +45,7 @@ function BuildingText({text, textNumber, position, size = 1, highlight = false} 
     const textMaterial = useMemo(() => new MeshStandardMaterial({color: color, emissive: color, toneMapped: false, emissiveIntensity: highlight ? 2 : 0 }), [highlight, color]);
 
     const font = new FontLoader().parse(Myfont);
-    const geometry = useMemo(() => new TextGeometry(verticalizeText(text), {font, size, letterSpacing, height:0.2}), [text]);
+    const geometry = useMemo(() => new TextGeometry(verticalizeText(text), {font, size, letterSpacing, depth:0.2}), [text]);
 
     useEffect(() => {
         if(refText.current) {
