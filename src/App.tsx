d3fccf1632c5f2c7d3e58a7f038f94688a39cb00
Guillaume Gomez <guillaume.gomez846@gmail.com>
2026-07-22 09:08:42 +0200

    fix linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import { useState } from 'react';
 import ThreeJsRendering from "./components/ThreeJsRendering";
 import useDate from "./useDate";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
   const { date } = useDate();
-
 
   return (
     <>
-      <ThreeJsRendering date={new Date()}/>
+      <ThreeJsRendering date={date}/>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
